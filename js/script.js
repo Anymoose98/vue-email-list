b9196ad5753f6e8b53173446c6096cad7b0f38e8
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-12-20 17:53:30 +0100

    Creato 10 email usando API

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      prova: 'Hello Vue!',
+      email: []
      
     }
   },
 
   created(){
-    
+    for(let i=0; i<10; i++){
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((email_generata) => {
+            console.log(email_generata.data.response);
+            this.email.push(email_generata.data.response);
+            console.log(this.email);
+        }
+        )
+    }
   },
 
   methods: {
